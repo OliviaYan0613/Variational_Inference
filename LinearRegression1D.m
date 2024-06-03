456cb0258f2292a9694ed57de6094dda629d2e14
OliviaYan0613 <cy2395@nyu.edu>
2024-06-03 19:42:30 -0400

    Add MCMC and compare calculated parameters

--- LinearRegression1D.m
+++ LinearRegression1D.m
@@ -2,21 +2,89 @@ clc;
 close all;
 clear global;
 
-% rng setup
-[s1,s2] = RandStream.create('mlfg6331_64','NumStreams',2);
-
-% Set up
+% Generate synthetic data
+rng(6); % For reproducibility
 n = 1e3;
-sigma2 = 1;
-beta = 0.3;
+%x = linspace(0, 100, n)';
+x = 10*randn(n,1);
+true_slope = 3;
+%true_intercept = 0;
+true_sigma = 1;
+%y = true_slope * x + true_intercept + true_sigma * randn(n, 1);
+rng(8);
+y = true_slope * x + true_sigma * randn(n, 1);
 
-% Data Generate
-x = randn(s1,1,n);
-err = sqrt(sigma2).*randn(s2,1,n);
-y = beta.*x + err;
+tau2 = 0.25;
 
-res = CAVI(x,y,0.25, 1e-2);
+res = CAVI(x,y,tau2, 1e-4);
+b_m = res(1);
+b_sd = res(2);
+sig_n = res(3);
+disp(res);
+disp(b_m);
+disp(b_sd);
+disp(sig_n);
 %test = elbo(x,y,0.36,0.02,111, 0.25,0);
+
+%---------------------------------------------------------------------------------------
+% MCMC sampling for beta and sigma^2
+num_samples = 1e4;
+burn_in = 1000;
+beta_samples = zeros(num_samples, 1);
+sigma2_samples = zeros(num_samples, 1);
+
+% Initial values for MCMC
+beta_current = randn;
+sigma2_current = 1;
+
+for i = 1:(num_samples + burn_in)
+    % Sample sigma^2 given beta and y
+    alpha_post = (n / 2) + 0.5;
+    beta_post = 0.5 * sum((y - beta_current * x).^2);
+    sigma2_current = 1 / gamrnd(alpha_post, 1 / beta_post);
+    
+    % Sample beta given sigma^2 and y
+    var_beta_post = 1 / (sum(x.^2) / sigma2_current + 1 / (tau2 * sigma2_current));
+    mean_beta_post = var_beta_post * (sum(x .* y) / sigma2_current);
+    beta_current = normrnd(mean_beta_post, sqrt(var_beta_post));
+    
+    % Store samples after burn-in
+    if i > burn_in
+        beta_samples(i - burn_in) = beta_current;
+        sigma2_samples(i - burn_in) = sigma2_current;
+    end
+end
+
+%----------------------------------------------------------------------------------
+% Plot the results
+figure;
+subplot(2,1,1);
+histogram(beta_samples, 'Normalization', 'pdf');
+title('Sample of \beta');
+xlabel('\beta');
+ylabel('Density');
+hold on;
+beta_x = linspace(min(beta_samples), max(beta_samples), 100);
+beta_y = normpdf(beta_x, mean(beta_samples), std(beta_samples));
+beta_elbo = normpdf(beta_x, b_m, sqrt(b_sd));
+plot(beta_x, beta_y, 'b', 'LineWidth', 2);
+plot(beta_x, beta_elbo, 'r','LineWidth', 2);
+legend('MCMC samples', 'Theoretical distribution','Distribution by ELBO');
+hold off;
+
+subplot(2,1,2);
+histogram(sigma2_samples, 'Normalization', 'pdf');
+title('Sample of \sigma');
+xlabel('\sigma');
+ylabel('Density');
+hold on;
+sigma2_x = linspace(min(sigma2_samples), max(sigma2_samples), 100);
+sigma2_y = normpdf(sigma2_x, mean(sigma2_samples), std(sigma2_samples));
+sigma2_elbo = gampdf(1./sigma2_x, (n+1)/2, 1/sig_n)./(sigma2_x.^2);
+plot(sigma2_x, sigma2_y, 'b', 'LineWidth', 2);
+plot(sigma2_x, sigma2_elbo, 'r', 'LineWidth', 2);
+legend('MCMC samples', 'Theoretical distribution', 'Distribution by ELBO');
+hold off;
 
 %-----------------------------------------------------------------------------------
 % Compute ELBO for example of 1D linear regression
@@ -122,10 +190,10 @@ function res = CAVI(x, y, tau2, epsilon)
             %disp(ELBO_new);
         end
     end
-    key = ["beta_mu", "beta_sd2", "nu", "ELBO"];
+    %key = ["beta_mu", "beta_sd2", "nu", "ELBO"];
     %val = {beta_mu, list_beta_sd2, list_nu, list_ELBO};
-    val_rst = {beta_mu,list_beta_sd2(end),list_nu(end),list_ELBO(end)};
-    res = dictionary(key, val_rst);
+    res = [beta_mu,list_beta_sd2(end),list_nu(end),list_ELBO(end)];
+    %res = dictionary(key, val_rst);
 end
 
 %----------------------------------------------------------------------------------------
