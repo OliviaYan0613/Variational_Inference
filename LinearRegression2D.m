7bd34a54b0258a9940cbdafa40398d51617d0d71
OliviaYan0613 <cy2395@nyu.edu>
2024-06-09 17:42:45 -0400

    Adjusted beta_sigma

--- LinearRegression2D.m
+++ LinearRegression2D.m
@@ -7,15 +7,16 @@ rng(6); % For reproducibility
 n = 1e4;
 x = 100*randn(n,2);
 %disp(x);
-true_slope = [1;1];
+true_slope = [2;3];
 %true_intercept = 0;
 noise = 1;
 rng(8);
 y = x * true_slope + noise * randn(n, 1);
 
 beta_pr_mu = [0; 0];
-%beta_pr_sigma2 = [0.01 0; 0 0.01];
-beta_pr_sigma2 = [0.1; 0.1]; % the first entry is sigma11 and the second entry is sigma22, sigma12 = sigma21 = 0
+beta_pr_sigma2_mx = [0.01 0; 0 0.01];
+beta_pr_sigma2 = [beta_pr_sigma2_mx(1,1); beta_pr_sigma2_mx(2,2)];
+%beta_pr_sigma2 = [0.01; 0.01]; % the first entry is sigma11 and the second entry is sigma22, sigma12 = sigma21 = 0
 
 %beta_mu = [3;1];
 %beta_sigma2 = [0.1;0.1];
@@ -26,8 +27,9 @@ disp(res);
 
 %-----------------------------------------------------------------------------------------------
 % Exact posterior
-sig_pr = [0.1 0; 0 0.1];
-mu_pr = [0;0];
+%sig_pr = [beta_pr_sigma2(1) 0; 0 beta_pr_sigma2(2)];
+sig_pr = beta_pr_sigma2_mx;
+mu_pr = beta_pr_mu;
 sig_post = inv(inv(sig_pr) + x'*x/noise);
 mu_post = (mu_pr'/sig_pr+y'*x/noise)/(inv(sig_pr)+x'*x/noise);
 
@@ -61,7 +63,7 @@ ylabel('\beta_2');
 legend('show');
 colorbar; % Add color bar for the contour levels
 
-saveas(gcf, 'comparisonLR2D.png');
+%saveas(gcf, 'comparisonLR2D.png');
 %----------------------------------------------------------------------------------
 
 function ELBO = elbo(x, y, beta_mu, beta_sigma2, beta_pr_mu, beta_pr_sigma2, noise)
@@ -91,30 +93,41 @@ function res = CAVI(x, y, beta_pr_mu, beta_pr_sigma2, noise)
     beta_sigma2_old = [1;1];
     ELBO_old = 0;
     ELBO_new = elbo(x, y, beta_mu_old, beta_sigma2_old, beta_pr_mu, beta_pr_sigma2, noise);
- 
+    list_ELBO = [ELBO_old];
+
     %beta_sigma2 (does not change during updating)
     beta_sigma2(1) = sum(x1.^2)/noise+beta_pr_sigma2(1);
     beta_sigma2(2) = sum(x2.^2)/noise+beta_pr_sigma2(2);
 
-     for i = 1:1e10
-         if (abs(ELBO_old - ELBO_new) >= epsilon)
+    for i = 1:100
+        if (abs(ELBO_old - ELBO_new) >= epsilon)
 
-            ELBO_old = ELBO_new;
+        ELBO_old = ELBO_new;
+        list_ELBO = [list_ELBO ELBO_new];
 
-            %update beta_mu
-            beta_mu_new(1) = (sum(y.*x1)/noise-sum(x1.*x2)*beta_mu_old(1)+beta_pr_mu(1)*beta_pr_sigma2(1))...
-                /beta_sigma2(1);
-            beta_mu_new(2) = (sum(y.*x2)/noise-sum(x1.*x2)*beta_mu_old(2)+beta_pr_mu(2)*beta_pr_sigma2(2))...
-                /beta_sigma2(2);
-             
-            %save beta_mu
-            beta_mu_old(1) = beta_mu_new(1);
-            beta_mu_old(2) = beta_mu_new(2);
-             
-            %updata ELBO
-            ELBO_new = elbo(x, y, beta_mu_old, beta_sigma2_old, beta_pr_mu, beta_pr_sigma2, noise);
-            %disp(ELBO_new);
-         end
-     end
-     res = {beta_mu_new; 1./beta_sigma2; ELBO_new; i};
+        %update beta_mu
+        beta_mu_new(1) = (sum(y.*x1)/noise-sum(x1.*x2)*beta_mu_old(1)+beta_pr_mu(1)*beta_pr_sigma2(1))...
+            /beta_sigma2(1);
+        beta_mu_new(2) = (sum(y.*x2)/noise-sum(x1.*x2)*beta_mu_old(2)+beta_pr_mu(2)*beta_pr_sigma2(2))...
+            /beta_sigma2(2);
+            
+        %save beta_mu
+        beta_mu_old(1) = beta_mu_new(1);
+        beta_mu_old(2) = beta_mu_new(2);
+            
+        %updata ELBO
+        ELBO_new = elbo(x, y, beta_mu_old, beta_sigma2_old, beta_pr_mu, beta_pr_sigma2, noise);
+        %disp(ELBO_new);
+        k = i;
+        end
+    end
+
+    figure;
+    elbo_x = 1:length(list_ELBO);
+    plot(elbo_x,list_ELBO, 'b', 'LineWidth', 2);
+    title('ELBO');
+    xlabel('iterations');
+    %hold on;
+
+    res = {beta_mu_new; 1./beta_sigma2; ELBO_new; k};
 end
